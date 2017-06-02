c4c7fff026fe16b2375b616cba5c93804cb15759
Vladimir Tisma <tishma@gmail.com>
2017-06-02 11:00:08 +0200

    cleaned up VacationRequestRepository

--- repository/VolatileVacationRequestRepository.php
+++ repository/VolatileVacationRequestRepository.php
@@ -3,30 +3,13 @@
 
 	namespace repository;
 
-
-	use model\Employee;
-
-
 	class VolatileVacationRequestRepository implements RepositoryInterface {
 
-		private $employees;
-
 		function __construct() {
-
-			$mark = Employee::create( "Mark Knopfler", 20 );
-			$joe  = Employee::create( "Joe Satriani", 20 );
-			$john = Employee::create( "John Petrucci", 20 );
-			$eric = Employee::create( "Eric Clapton", 20 );
-
-			$this->employees   = [];
-			$this->employees[] = $mark;
-			$this->employees[] = $joe;
-			$this->employees[] = $john;
-			$this->employees[] = $eric;
 
 		}
 
 		public function findById( int $id ) {
-			return $this->employees[ $id ];
+			return null;
 		}
 	}
